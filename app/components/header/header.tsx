5ebc0c76840f67e28924a0fe9f668fb4f19fe950
a6hi <saraffabhishek@outlook.com>
2025-09-03 00:54:22 -0400

    updated header, added hero section

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -1,10 +1,78 @@
-import LogoText from '../../../public/predivine-logo-text-transparent.png'
+import styles from "./header.module.css";
+import Image from "next/image";
+import LogoText from "../../../public/predivine-logo-text-transparent.png";
 
 export default function Header() {
   return (
-      <div className="header">
-        <div className="logo">LogoText</div>
-        <div className="menu"></div>
+    <div className="header">
+      {/* Logo */}
+      <div className="navbar py-3 bg-base-100 shadow-xl fixed top-0 z-50">
+        <div className="navbar-start">
+          <a>
+            <Image
+              src={LogoText}
+              alt="Predivine Logo"
+              width={250} // pick a size
+              height={60}
+              className="min-w-[100px] max-w-[200px] h-auto w-auto"
+              priority
+            />
+          </a>
+        </div>
+
+        <div className="navbar-end">
+          {/* Menu */}
+          <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal px-1 pr-5 link-text">
+              <li>
+                <a>About</a>
+              </li>
+              <li>
+                <a>Services</a>
+              </li>
+              <li>
+                <a>FAQ</a>
+              </li>
+            </ul>
+          </div>
+
+          {/* Dropdown Menu */}
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16 M4 12h16 M4 18h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu dropdown-content absolute inset-0 z-50 w-full h-screen bg-base-100 flex flex-col items-center justify-center p-8"
+            >
+              <li>
+                <a>About</a>
+              </li>
+              <li>
+                <a>Services</a>
+              </li>
+              <li>
+                <a>FAQ</a>
+              </li>
+            </ul>
+          </div>
+
+          <a className={`btn px-3 py-7 link-text ${styles.contactUsBtn}`}>Contact Us</a>
+        </div>
       </div>
+    </div>
   );
 }
--- app/components/hero/hero.tsx
+++ app/components/hero/hero.tsx
@@ -1,7 +1,34 @@
 export default function Hero() {
   return (
-      <div className="hero">
-        The Top CS Tutor in Toronto
+    <div className="hero">
+      <div
+        className="hero hero-text mt-20"
+        style={{
+          backgroundImage:
+            "url(https://cdn.pixabay.com/photo/2022/08/30/20/47/institute-7421918_1280.jpg)",
+          height: "92vh",
+        }}
+      >
+        <div className="hero-overlay"></div>
+        <div className="hero-content text-neutral-content text-center">
+          <div className="max-w-4xl">
+            <p className="mb-5">The Best CS Tutors in Toronto.</p>
+            <button className="btn button-text accent px-12 py-6">
+              Book a Free Call
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 32 32"
+                fill="currentColor"
+                className="h-6 w-6 scale-75"
+              >
+                <path d="M13.216 8.064C12.834 3.67 8.25 1.514 8.056 1.426A1.01 1.01 0 0 0 7.474 1.348c-5.292.878-6.088 3.958-6.12 4.086a1.01 1.01 0 0 0 .02.54c6.312 19.584 19.43 23.214 23.742 24.408.332.092.606.166.814.234.1.034.204.048.308.048.142 0 .284-.03.414-.09.132-.06 3.248-1.528 4.01-6.316a1 1 0 0 0-.096-.612c-.068-.132-1.698-3.234-6.218-4.33a1 1 0 0 0-.884.21c-1.426 1.218-3.396 2.516-4.246 2.65-5.698-2.786-8.88-8.132-9-9.146 1.348-1.336 2.654-3.238 4.156-4.866a1 1 0 0 0 .26-.686z" />
+              </svg>
+            </button>
+          </div>
+        </div>
       </div>
+    </div>
   );
 }
