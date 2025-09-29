397f6ba0d83a2d9f56dc811ce95f081706e585db
a6hi <saraffabhishek@outlook.com>
2025-09-29 02:59:16 -0400

    fixed bug where logo would not appear in front of contact

--- app/components/contactBanner/contactBanner.tsx
+++ app/components/contactBanner/contactBanner.tsx
@@ -11,7 +11,7 @@ export default function ContactBanner() {
           width="16"
           height="16"
           fill="currentColor"
-          className="text-[var(--text-accent)]"
+          className="text-white"
           viewBox="0 0 16 16"
         >
           <path
@@ -19,19 +19,19 @@ export default function ContactBanner() {
             d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"
           />
         </svg>
-        <a href="tel:6475474808" aria-label="Call Predivine CS Tutoring">
+        <a href="tel:6475474808">
           647.547.4808
         </a>
       </span>
 
-      {/* Email */}
+      {/* Email SVG */}
       <span className="flex items-center gap-1">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="16"
           height="16"
           fill="currentColor"
-          className="text-[var(--text-accent)]"
+          className="text-white"
           viewBox="0 0 16 16"
         >
           <path
